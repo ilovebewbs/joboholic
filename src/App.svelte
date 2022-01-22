19871aa8b4e46c23f453801824e38cd8f755c8d7
ilovebewbs <sruwhfew@gmail.com>
2022-01-22 23:55:16 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+import { each } from "svelte/internal";
+
+import NewsItem from "./components/news_item.svelte";
+let news_stuff = [];
+(async()=>{
+	let res = await fetch("https://api.pushshift.io/reddit/search/submission/?subreddit=javascript_jobs");
+	let json = await res.json();
+	let stuff = json.data;
+	stuff.map((el)=>{
+		let title = el.title;
+		let id = el.id;
+		let author = el.author;
+		let url = el.url;
+		let score = el.score;
+		let time_posted = new Date(el.created_utc*1000).toLocaleString();
+		news_stuff = [...news_stuff,{title,author,score,time_posted,url,id}];
+	})
+})();
+</script>
+<div class="container text-center">
+	<h1> Tech jobs  </h1>
+	<p>I built this simple app to learn svelte and will be adding more features to it as i learn more</p>
+	<div>
+		<a href="github.com">
+			<img src="https://img.icons8.com/color/48/000000/github.png" alt="github">
+		</a>
+		<a href="discord.com">
+			<img src="https://img.icons8.com/color/48/000000/discord.png" alt="discord">
+		</a>
+	</div>
+</div>
+<main>
+	{#each news_stuff as item (item.id) }
+		<NewsItem {item}/>
+	{/each}
+</main>
---
+++ src/components/news_item.svelte
@@ -0,0 +1,53 @@
+<script>
+    export let item;
+    const favThis = (item)=>{
+        console.log(item.title);
+    }
+</script>
+
+<div class="feed-item">
+    <img class="js-icon"src="https://img.icons8.com/color/48/000000/javascript--v1.png" alt=""/>
+   <h3>{item.title}</h3>
+   <div>
+    <a href={item.link} class="story-link" >Read More</a>
+    <button on:click={()=>favThis(item)} class="favbtn">
+        Add to favorites
+    </button>
+   </div>
+   <div class="details">
+       <b>{item.time_posted} | {item.author} | {item.score} upvotes</b>
+   </div>
+</div>
+
+<style>
+    .feed-item{
+        margin: 15px;
+        padding: 30px;
+        background-color:lightblue;
+        color: darkmagenta;
+        border-radius: 25px;
+        position: relative;
+    }
+    .story-link{
+        background-color: darkmagenta;
+        color: lightblue;
+        padding: 10px;
+        margin: 5px 5px;
+        border-radius: 20px;
+    }
+    .details{
+        font-size: 12px;
+        color: darkmagenta;
+        padding: 10px 0px 10px 0px;
+    }
+    .js-icon{
+        border-radius: 50px;
+        position: absolute;
+        right: 15px;
+        bottom: 10px;
+    }
+    .favbtn {
+        padding: 10px;
+        border-radius: 20px;
+    }
+</style>
